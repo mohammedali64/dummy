6a913e0e48128bc907632fc4bb735b4ec76f8c1a
mohammedali64 <mohammedali88870@gmail.com>
2025-05-26 22:24:55 +0530

    Update dummy.js

--- dummy.js
+++ dummy.js
@@ -1,1 +1,12 @@
-console.log("I love JavaScript");
+// Write your code below:
+const mainHeading = document.getElementById('main-heading');
+mainHeading.style.color = 'orange';
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom = '2px solid orange';
+const basketHeading = document.getElementById('basket-heading');
+basketHeading.style.color = 'green';
+const thanksDiv = document.getElementById('thanks');
+const paragraph = document.createElement('p');
+paragraph.textContent = 'Please visit us again';
+thanksDiv.appendChild(paragraph);
